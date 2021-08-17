1510409516aab1a40e866803711760e9e19cc301
Karan kanchan <kanchan.karan.17ce7002@gmail.com>
2021-08-17 17:09:19 +0530

    printing the interger,char and double array

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace GenericsUsercase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+            char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+
+            Program.toPrint(intArray);
+            Program.toPrint(charArray);
+            Program.toPrint(doubleArray);
+        }
+        
+        public static void toPrint(double[] inputArray)
+        {
+            foreach(double element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("___________________");
+        }
+
+        public static void toPrint(int[] inputArray)
+        {
+            foreach(int element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("___________________");
+        }
+
+        public static void toPrint(char[] inputArray)
+        {
+            foreach(char element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("_______________________");
+        }
+
+    }
+}
